7ccb60935e11ec54e5e7eee5eb5660c7cd4edb78
Syeda Sabahat <159911882+sabahat97@users.noreply.github.com>
2024-03-22 01:33:15 +0500

    Add files via upload

---
+++ return.ts
@@ -0,0 +1,92 @@
+//returning function value
+
+function alphbet(): number {
+    let a = 12 + 45; //output 57
+    return a;
+  }
+  //invoking the function
+  let response: number = alphbet();
+  console.log(response);
+  
+  //ex 2
+  function halffryegg(): number {
+    let coocked = 1 + 1.8 + 2; //output 4.8
+    return coocked;
+  }
+  //invoking the function
+  let x: number = halffryegg();
+  console.log(x);
+  
+  //ex 3
+  
+  function car(): string {
+    let carName = "Civic Rs Turbo"; // output Civic Rs Turbo
+    return carName;
+  }
+  //invoking the
+  let cars: string = car();
+  console.log(cars);
+  
+  //ex 4
+  
+  function sum(): number {
+    let summ = 12 + 12; //output 24
+    return summ;
+  }
+  let addition: number = sum();
+  console.log(addition);
+  
+  //ex 5
+  function sunglasses(): string {
+    let glasess = " shade of sun glasess is Blackblue, Greyred";
+    return glasess;
+  }
+  let sunglases: string = sunglasses();
+  console.log(sunglases);
+  
+  //ex 6
+  function multi(): number {
+    let pluse = 34 + 38;
+    return pluse;
+  }
+  let plus: number = multi();
+  console.log(plus);
+  
+  //ex 7
+  
+  function chair(): string {
+    let color = "black, red"; //black , red
+    return color;
+  }
+  //invoking the function
+  let c: string = chair();
+  console.log(c);
+  
+  //ex 8
+  function email(): boolean {
+    let isId = true; //true
+    return isId;
+  }
+  //invoking the function
+  let id: boolean = email();
+  console.log(id);
+  
+  //ex 9
+  function mobile() {
+    let model = "Iphone 14 pro";  //Iphone 14 pro
+    return model;
+  }
+  //invoking the function
+  let phone = mobile();
+  console.log(phone);
+  
+  //ex 10
+  
+  function code(): boolean {
+    let coding = true;   //true
+    return coding; 
+  }
+  //invoking the function
+  let cod: boolean = code();
+  console.log(cod);
+  
